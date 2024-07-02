5f427510f4a1ba9ba718e6b77255e33a595c213c
Pohsiang Wang-王柏翔-精誠-全流通應用整合業務處 <2400389@systex.com.tw>
2024-07-02 16:13:50 +0800

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { jsPDF } from 'jspdf';
+import {myFontData} from './font';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,23 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  myfont : string = myFontData;
   title = 'pdftest';
+  createPDF(): void {
+    const doc = new jsPDF();
+    
+    // Example of adding a font directly (this should be your actual Base64 string)
+    const fontBase64 = this.myfont;
+    doc.addFileToVFS('NotoSansTC-VariableFont_wght-normal.ttf', fontBase64);
+    doc.addFont('NotoSansTC-VariableFont_wght-normal.ttf',
+    'NotoSansTC-VariableFont_wght',
+    'normal');
+    doc.setFont('NotoSansTC-VariableFont_wght');
+
+  doc.text('測試',10,10);
+  // 保存 PDF 文件
+  doc.save('todolist.pdf');
+
+
+  }
 }
